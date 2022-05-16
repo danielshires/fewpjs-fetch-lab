569a92463d3b2491988c21d6bb47b8a62be8973c
danielshires <hello@danielshires.com>
2022-05-16 12:49:10 +1000

    Assignment complete

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+    // To pass the tests, don't forget to return your fetch!
+    const url = 'https://anapioficeandfire.com/api/books'
+    return fetch(url)
+        .then(response => response.json())
+        .then(json => {
+            console.log(json[4])
+            console.log(json[4].characters[1030])
+            let i = 0
+            let pageCount = []
+            while (i < json.length) {
+                pageCount.push(json[i].numberOfPages)
+                i++
+            }
+            let add = (a, b) => a + b
+            const sum = pageCount.reduce(add)
+            console.log(sum)
+            renderBooks(json)
+
+        })
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+    const main = document.querySelector('main');
+    books.forEach(book => {
+        const h2 = document.createElement('h2');
+        h2.innerHTML = book.name;
+        main.appendChild(h2);
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+    fetchBooks();
 });
